a2d42413b83ef05e85437d441021ccdaa7232bd4
Aditya Chandavale <aa.chandavale@gmail.com>
2021-08-03 07:56:08 +0530

    Added Pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,17 +6,29 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/home')
+def home():
+    return redirect(url_for('index'))
+
 @app.route('/featured')
 def featured():
     return render_template('featured.html')
 
-@app.route('/achievements')
+@app.route('/acheivements')
 def achievements():
-    return render_template('achievements.html')
+    return render_template('acheivements.html')
 
 @app.route('/skills')
 def skills():
     return render_template('skills.html')
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
